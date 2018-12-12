b3e0b610acf14987485c788c612a4b796761b0a3
TG-Techie <39284876+TG-Techie@users.noreply.github.com>
2018-12-11 21:50:37 -0500

    Update boot.py

--- system/boot.py
+++ system/boot.py
@@ -8,7 +8,7 @@ import time
 from tg_io.io_screen import disp
 
 disp.text(0,6*8,'''--------
-TG-US Version: ''' + str(0)+'''
+TG-US Version: ''' + '1.x.Alpha.0'+'''
 
 Created By:
 TG-Techie - Jonah Y-M
